cd0f8f9c50bc2c3ada3f19460a3055d89f1cb3ba
XinBao321 <xinbaoc2@nycstudents.net>
2021-01-24 08:39:37 -0500

    Finish with some of the methods

--- 00-SuperArray/Driver.java
+++ 00-SuperArray/Driver.java
@@ -1,5 +1,24 @@
 public class Driver{
   public static void main(String[] args) {
-    SuperArray array1 = new SuperArray(11);
+    SuperArray array1 = new SuperArray(8);
+    array1.add(100);
+    array1.add(35);
+    array1.add(62);
+    array1.add(33);
+    array1.add(50);
+    array1.add(92);
+    array1.add(72);
+    array1.add(12);
+    array1.add(32);
+    array1.add(92);
+    array1.add(72);
+    array1.add(102);
+
+    System.out.println(array1);
+    array1.add(3,661);
+    System.out.println(array1);
+    array1.grow(3);
+    System.out.println(array1);
+    System.out.println(array1.isEmpty());
   } //end main
 } //end class
--- 00-SuperArray/SuperArray.java
+++ 00-SuperArray/SuperArray.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 import java.util.Arrays;
 
   public class SuperArray{
+    Scanner scan = new Scanner(System.in);
     //instance variables
+
     private int[] array;
-    private numberCount;
+    private int numberCount;
+
+  public SuperArray(int length){
+    this.array = new int[length];
+  } //end specific size array
 
   public SuperArray(){
     this.array = new int[10];
@@ -12,24 +18,66 @@ import java.util.Arrays;
 
   public void add(int val){
 
-  } //end first add method
+    //Firse case which the array is not filled up yet
+    for(int i = 0; i < array.length; i++){
+      if(array[i] == 0){
+        this.array[this.numberCount] = val;
+        this.numberCount++;
+        return;
+      }
+    }
+    //Second case occur when the array is filled up and need to make a new one
+      int [] array2 = new int[array.length + 1];
+        for(int i = 0; i < array.length; i++){
+         array2[i] = array[i];
+      }
+      array2[array2.length - 1] = val;
+      array = new int[array2.length];
+      array = array2;
+    } //end first add method
 
   public void add(int index, int val){
-    this.array[index] = val;
-  } //end second add method
+
+    for(int i = 0; i < array.length; i++){
+        int number = array[index];
+
+      if(this.array[index] != 0) {
+        this.array[index] = val;
+        }
+        /*Still trying to figure it out
+        //System.out.println(number);
+        int [] array2 = new int[array.length + 1];
+        //array2[i] = array[i];
+        array2[index] = number;
+        array2[array2.length - 1] = val;
+        array = new int[array2.length];
+        array = array2;
+      }
+      */
+}
+
+}//end second add method
 
   public void grow(int n){
-
+     int growSize = this.array.length;
+    int [] array = new int [n + growSize];
   } //end grow method
 
   public void set(int i, int val){
-
+    if (this.array[i] != 0) {
+    this.array[i] = val;
+   } //end if the value is not 0
+  this.numberCount += i;
   } //end set method
 
   public boolean isEmpty() {
-  return true;
-
-  } //end isEmpty method
+    for (int i = 0; i < array.length; i++) {
+          if(array[i] != 0) {
+            return false;
+          }
+      }
+        return true;
+    } //end isEmpty method
 
   public void remove(int index) {
 
