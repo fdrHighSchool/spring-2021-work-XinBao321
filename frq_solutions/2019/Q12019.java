6a7414d6990577b2a19b6d5e3a3e2f5fce86189e
XinBao321 <>
2021-03-05 10:09:22 -0500

    Fixes mistakes in 2019

--- frq_solutions/2019/Q12019.java
+++ frq_solutions/2019/Q12019.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
     private boolean [][] lights;
 
     public LightBoard(int numRows, int numColumns){
+      light = new boolean [numRows][numColumns];
       int max = 10;
       int min = 0;
       int range = max - min + 1;
@@ -13,23 +14,29 @@ import java.util.Arrays;
            //System.out.println(rand);
            if(random >= 4 ){
             return this.lights[numRows,numColumns] false;
-           }
-        }
-      }
-    }
+          } //end if
+        } //end second for loop
+      } //end for loop
+
+      boolean [][] lights = this.lights;
+    } //end LightBoard method
 
     public boolean evaluateLight(int row, int col){
       int count;
       for (int i = 0; i < lights.length; i++){
         if(lights[i,col]){
           count++;
-        }
-      }
-      if(count % 3 != 0){
-        return false;
-      }
-      else if (count % 2 == 0){
+        } //end if
+      } //end for loop
+      if(!this.light[row][col] == true){
+        if(count % 3 == 0){
+        return true;
+      } //end if statement
+    } // end if statement
+      else if(this.light[row][col]){
+        if(count % 2 == 0){
            return false;
-    }
+    } //end if
+  } // end else statement
       return lights[row][col];
-      }
+    } //end evaluate light method
